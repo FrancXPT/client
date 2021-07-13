358a84e9066156415ffa8a7a0565d494f40367c4
Achraf Bamoula <30707191+FrancXPT@users.noreply.github.com>
2021-07-13 10:03:48 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `dreamspa_notch_demo`
+-- Database: `dreamspa_renews_demo`
 --
 
 -- --------------------------------------------------------
